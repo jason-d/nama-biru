fec37a32714f8613c2132ee16b50f5530c4f13b0
Jason <jason.d.ody@gmail.com>
2013-09-01 01:24:41 -0400

    Express setup

---
+++ App/Index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+var notFoundHandler = function(req, res) {
+    res.statusCode = 404;
+    res.description = 'Page not found.';
+    res.render('notFound');
+};
+
+var errorHandler = function(err, req, res, next) {
+    console.log("****ERROR****");
+    console.log(err);
+    
+    res.statusCode = 500;
+    res.description = 'Something went wrong';
+    res.render('error');
+};
+
+app.configure(function() {
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/Views');
+    app.use(express.static(path.join(__dirname, 'Public')));
+    app.use(app.router); //Enable error handling
+    app.use(notFoundHandler); //If not routes match, this will be called
+    app.use(errorHandler); //Express knows a method with 4 params, is for handling errors
+});
+
+app.listen(process.env.PORT, process.env.IP);
+console.log('Nama-biru running...');
